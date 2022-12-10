380ab3909bf8bdcc630de1341940ffb609000ce3
окси <83812355+oxijoined@users.noreply.github.com>
2022-12-10 12:29:32 +0300

    release

---
+++ main.py
@@ -0,0 +1,42 @@
+import telebot
+import os
+import random
+from simpledemotivators import Demotivator
+
+
+bot = telebot.TeleBot(
+    "5661752388:AAE07pQHkT3IPpYoRdkaNoOqIVmmuSoKw5Y", parse_mode="HTML",skip_pending=True
+)
+
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    bot.reply_to(message, "<b>Привет! Отправь фото и я сделаю из него демотиватор!</b>\n<code>Ты можешь добавить число к фотографии, чтобы увеличить число итераций</code>")
+
+def get_text():
+    with open('names.txt','r',encoding='utf-8') as f:
+        text = [x for x in f]
+    return random.choice(text)
+
+@bot.message_handler(content_types=["photo"])
+def photo_processing(message):
+    fileID = message.photo[-1].file_id
+    file_info = bot.get_file(fileID)
+    downloaded_file = bot.download_file(file_info.file_path)
+    with open(f"{fileID}.jpg", "wb") as new_file:
+        new_file.write(downloaded_file)
+    if message.caption.isdigit():
+        if int(message.caption) <= 10:
+            for i in range(int(message.caption)):
+                dem = Demotivator(get_text(), get_text())
+                dem.create(f"{fileID}.jpg", result_filename=f"{fileID}.jpg")
+    else:
+        dem = Demotivator(get_text(), get_text())
+        dem.create(f"{fileID}.jpg", result_filename=f"{fileID}.jpg")
+    with open(f"{fileID}.jpg", "rb") as photo:
+        bot.send_photo(
+            message.chat.id, photo, caption="Сделано в\nt.me/Demotivatorokcubot"
+        )
+    os.remove(f"{fileID}.jpg")
+
+bot.polling()
