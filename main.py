309bb7f4ad8ac0cda7e458e87c0b999ad1518cdc
окси <83812355+oxijoined@users.noreply.github.com>
2022-12-10 12:35:03 +0300

    fix 3

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from simpledemotivators import Demotivator
 
 
 bot = telebot.TeleBot(
-    "5661752388:AAE07pQHkT3IPpYoRdkaNoOqIVmmuSoKw5Y", parse_mode="HTML",skip_pending=True
+    "5661752388:AAE07pQHkT3IPpYoRdkaNoOqIVmmuSoKw5Y", parse_mode="HTML", skip_pending=True
 )
 
 
@@ -13,10 +13,12 @@ bot = telebot.TeleBot(
 def start(message):
     bot.reply_to(message, "<b>Привет! Отправь фото и я сделаю из него демотиватор!</b>\n<code>Ты можешь добавить число к фотографии, чтобы увеличить число итераций</code>")
 
+
 def get_text():
-    with open('names.txt','r',encoding='utf-8') as f:
+    with open('names.txt', 'r', encoding='utf-8') as f:
         text = [x for x in f]
     return random.choice(text)
+
 
 @bot.message_handler(content_types=["photo"])
 def photo_processing(message):
@@ -29,18 +31,22 @@ def photo_processing(message):
         if int(message.caption) <= 10:
             for i in range(int(message.caption)):
                 dem = Demotivator(get_text(), get_text())
-                dem.create(f"{fileID}.jpg", result_filename=f"{fileID}.jpg",watermark='t.me/Demotivatorokcubot')
+                dem.create(f"{fileID}.jpg", result_filename=f"{fileID}.jpg",
+                           watermark='t.me/Demotivatorokcubot')
         else:
             for i in range(10):
                 dem = Demotivator(get_text(), get_text())
-                dem.create(f"{fileID}.jpg", result_filename=f"{fileID}.jpg",watermark='t.me/Demotivatorokcubot')
+                dem.create(f"{fileID}.jpg", result_filename=f"{fileID}.jpg",
+                           watermark='t.me/Demotivatorokcubot')
     else:
         dem = Demotivator(get_text(), get_text())
-        dem.create(f"{fileID}.jpg", result_filename=f"{fileID}.jpg",watermark='t.me/Demotivatorokcubot')
+        dem.create(f"{fileID}.jpg", result_filename=f"{fileID}.jpg",
+                   watermark='t.me/Demotivatorokcubot')
     with open(f"{fileID}.jpg", "rb") as photo:
         bot.send_photo(
             message.chat.id, photo, caption="Сделано в\nt.me/Demotivatorokcubot"
         )
     os.remove(f"{fileID}.jpg")
+
 
 bot.polling()
